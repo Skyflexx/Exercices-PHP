935bf5c8192e12248baa676fcff4e9cb993306aa
Skyflexx <118121459+Skyflexx@users.noreply.github.com>
2023-03-21 16:52:24 +0100

    Add files via upload

--- exo10.php
+++ exo10.php
@@ -17,57 +17,47 @@
 
     $montantDu = 163;
     $montantVerse = 300;
-    $resteDu = $montantVerse - $montantDu; // calcul de la différence
+    $resteDu = $montantVerse - $montantDu; 
     
     echo "Montant à payer : $montantDu € <br> Montant versé : $montantVerse € <br> Reste du : $resteDu €<br>";
 
 
-// NOMBRE DE BILLETS DE 10
+// CALCUL DU NOMBRE DE BILLETS DE 10
      
 
     $facto = intdiv($resteDu, 10); // Variable qui sera utilisée pour diviser par 10, 5, 2 et 1
 
     $dix = floor($facto);   // Contient le nombre de billet de 10, sans la virgule avec Floor
 
-    echo "$dix billets de 10€"; // Affichage du resultat.
-
     $resteDu = $resteDu - ($dix * 10); // Recalcul du reste du en retirant le nombre de billets de 10
 
 
- // NOMBRES DE BILLETS DE 5   
+ // CALCUL DU NOMBRE DE BILLETS DE 5   
 
 
     $facto = intdiv($resteDu, 5); // On redivise par 5 pour obtenir le nombre de billet de 5
 
     $cinq = floor($facto); // On obtient ainsi le nombre de billets en retirant ce qu'il y a après la virgule
 
-    echo " - $cinq billets de 5€ "; // affichage
-
     $resteDu = $resteDu - ($cinq * 5); 
 
    
-// NOMBRES DE PIECES DE 2
+// CALCUL DU NOMBRE DE PIECES DE 2
 
     $facto = intdiv($resteDu, 2);
 
     $deux = floor($facto);   
 
-    echo " - $deux pièces de 2 € ";
-
-
-// NOMBRE DE PIECES DE 1
-
-
-
     $resteDu = $resteDu - ($deux * 2);
+
+
+// CALCUL DU NOMBRE DE PIECES DE 1  
 
     $facto = intdiv($resteDu, 1);
 
-    $un = floor($facto); 
-
-    echo " - $un pièces de 1€ ";
+    $un = floor($facto);     
     
-    
+    echo "Rendu de monnaie : <br> $dix billets de 10€ - $cinq billets de 5€ - $deux pièces de 2 € - $un pièces de 1€ "
 
 ?>
 
